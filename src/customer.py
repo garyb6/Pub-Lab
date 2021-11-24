23392336f1cbfc13538d001bc82356bcd22c26c4
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-24 12:15:21 +0000

    Added test and method for the customer to buy drink from pub

--- src/customer.py
+++ src/customer.py
@@ -3,3 +3,12 @@ class Customer:
         self.name = name
         self.stomach = stomach
         self.wallet = wallet
+
+    def buy_drink(self, pub, drink_name):
+        drink = pub.find_drink_by_name(drink_name)
+
+        self.stomach.append(drink)
+        self.wallet -= drink.price
+
+        pub.till += drink.price
+        pub.drinks.remove(drink)
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -9,7 +9,7 @@ class TestCustomer(unittest.TestCase):
     def setUp(self):
         self.drink1 = Drink('Whisky', 5.00, True)
         self.drink2 = Drink('Whisky', 5.00, True)
-        self.customer = Customer("Stephen O'Reilly", [self.drink1, self.drink2], 2.00)
+        self.customer = Customer("Stephen O'Reilly", [self.drink1, self.drink2], 6.00)
         
     def test_customer_has_name(self):
         self.assertEqual("Stephen O'Reilly", self.customer.name)
@@ -18,4 +18,15 @@ class TestCustomer(unittest.TestCase):
         self.assertEqual(2, len(self.customer.stomach))
 
     def test_customer_has_wallet(self):
+        self.assertEqual(6.00, self.customer.wallet)
+
+    def test_customer_can_buy_drink(self):
+        self.drink3 = Drink('Beer', 4.00, True)
+        self.pub = Pub('The Prancing Pony', 100.00, [self.drink1, self.drink2, self.drink3])
+
+        self.customer.buy_drink(self.pub, 'Beer')
+
+        self.assertEqual(3, len(self.customer.stomach))
         self.assertEqual(2.00, self.customer.wallet)
+        self.assertEqual(104.00, self.pub.till)
+        self.assertEqual(2, len(self.pub.drinks))
