e12684dcd0ae0a574564927e7c843d96bc69abef
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-24 16:23:50 +0000

    filled class and ran initialisation tests

--- tests/food_tests.py
+++ tests/food_tests.py
@@ -4,4 +4,15 @@ from src.food import Food
 
 class TestFood(unittest.TestCase):
     def setUp(self):
-        self.food = ("Crisps", 1.00, 0.5)
+        self.food = Food("Crisps", 1.00, 0.5)
+    
+    def test_food_has_name(self):
+        self.assertEqual('Crisps', self.food.name)
+
+    def test_food_has_price(self):
+        self.assertEqual(1.00, self.food.price)
+
+    def test_food_has_rejuvenation_level(self):
+        self.assertEqual(0.5, self.food.rejuvenation_level) 
+    
+    
